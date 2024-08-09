c3f99ac18da585f7fe7f2625c7f996e68c9d6da8
Dogus <49583100+emmtsec@users.noreply.github.com>
2024-08-09 18:47:42 +0300

    Update tahminoyunu.py

--- 4- tahmin oyunu/tahminoyunu.py
+++ 4- tahmin oyunu/tahminoyunu.py
@@ -1,57 +1,52 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import random
 import os
 
-def clearmenu():
+def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-class start(object):
+class Game:
     def __init__(self):
-        clearmenu()
-        print("\n Welcome to TAHMINOYUNU\n 1- Başla\n 2- Zor \n 3- Çıkış")
+        clear_screen()
+        print("\nWelcome to TAHMINOYUNU")
+        print("1- Başla\n2- Zor\n3- Çıkış")
+
+    def play(self, max_attempts=None):
+        rand = random.randint(1, 100)
+        counter = 0
+        while True:
+            counter += 1
+            try:
+                guess = int(input(f"\n(1-100 arası sayı giriniz{' Unutmayın sadece 5 canınız var!' if max_attempts else ''}) > Tahminizi giriniz: "))
+                if guess > rand:
+                    print("Yaklaştın, sayı düşürünüz!")
+                elif guess < rand:
+                    print("Yaklaştın, sayı yükseltiniz!")
+                else:
+                    print(f"Tebrikler, {counter} denemede buldunuz!")
+                    break
+
+                if max_attempts and counter >= max_attempts:
+                    print(f"Bütün tahmin hakkınızı kullandınız! Oyun bitti. Sayı şuydu: {rand}")
+                    break
+
+            except ValueError:
+                print("Lütfen geçerli bir sayı giriniz.")
 
 if __name__ == '__main__':
-    start()
-    rand = random.randint(1,100)
-    counter = 0
+    game = Game()
     choice = input("Seçiminiz: ")
     if choice.isdigit():
         choice = int(choice)
 
         if choice == 1:
-            while True:
-                counter += 1
-                closs = input("\n(1-100 arası sayı giriniz) > Tahminizi giriniz: ")
-                if int(closs) > rand:
-                    print("Yaklaştın sayı düşünüz!")
-                elif int(closs) < rand:
-                    print("Yaklaştın sayı çıkınız!")
-                elif int(closs) == rand:
-                    print("Tebrikler buldunuz şu kadar {} sayıda tahmin ettiniz..".format(counter))
-                    break
-
+            game.play()
         elif choice == 2:
-            while True:
-                counter += 1
-                closs = input("\n(1-100 arası sayı giriniz Unutmayın sadece 5 canınız var!) > Tahminizi giriniz: ")
-                if counter == 5:
-                    print("Bütün tahmin hakkınızı kullandınız! Oyun bitti.. Tahmin sayısı şudur > {}".format(rand))
-                    break
-                elif int(closs) > rand:
-                    print("Yaklaştın sayı düşünüz!")
-                elif int(closs) < rand:
-                    print("Yaklaştın sayı çıkınız!")
-                elif int(closs) == rand:
-                    print("Tebrikler buldunuz şu kadar {} sayıda tahmin ettiniz..".format(counter))
-                    break
-
+            game.play(max_attempts=5)
         elif choice == 3:
-            pass
-
+            print("Oyun kapatılıyor.")
         else:
-            print("1, 2 ve 3 sayılarından birini seçiniz..")
-            pass
-
+            print("1, 2 veya 3 sayılarından birini seçiniz.")
     else:
-        print("Harf girmeyiniz!")
+        print("Lütfen bir sayı giriniz.")
